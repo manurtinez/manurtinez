83904ceadc9055633f75bb14eb70f73384970250
Manuel Martinez <manuel.martinez@devsar.com>
2021-01-01 21:37:39 -0300

    progress...

---
+++ components/ProfileImage.vue
@@ -0,0 +1,11 @@
+<template>
+  <img class="profile-image" src="~/assets/profile_image.jpg" />
+</template>
+
+<style scoped>
+.profile-image {
+  height: 50%;
+  width: 50%;
+  border-radius: 50%;
+}
+</style>
